9178ef1a09a18b399153f9cbca7fd3c5335cbdab
WeLL DeKuR <sunnyx2442@gmail.com>
2025-03-17 22:32:53 +0100

    Update Config.py

--- Program/Config/Config.py
+++ Program/Config/Config.py
@@ -2,9 +2,9 @@
 
 name_tool       =  "Well"
 type_tool       =  "Multi-Tools"
-version_tool    =  "1.4"
+version_tool    =  "2.0"
 coding_tool     =  "Python 3"
-language_tool   =  "EN"
+language_tool   =  "EN/FR"
 creator         =  "Well Dekur"
 platform        =  "Windows 10/11 & Linux"
 github_tool     =  "          𝐁𝐲 𝐖𝐞𝐋𝐋 𝐃𝐞𝐤𝐮𝐑"
